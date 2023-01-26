0a2794c493202138ca74065468bb40eb47ff6a1b
Raven Xyzer <raynezzar21@gmail.com>
2023-01-26 11:21:41 +0700

    Minor Update - Nothing important just make its cleaner.

--- src/Khemia.ts
+++ src/Khemia.ts
@@ -1,20 +1,12 @@
 import "dotenv/config";
-import { container, SapphireClient } from "@sapphire/framework";
-import { GuildTextBasedChannel, Message } from "discord.js";
-import { Queue, Song, Playlist } from "distube";
 
-import { IClient } from "./structures/builds/IClient";
-import { Emojis, Utils } from "./libraries";
+import { IClient } from "./structures";
 
 /**
  * @description Mongoose set query
  */
 import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
-
-/**
- * @description Distube client
- */
 
 /**
  * @description Khemia login
